832740ff18d68cab42dd510edba0904cb8cc9397
ameya-6964 <belvalkar7@gmail.com>
2024-03-08 16:22:25 +0530

    Added External App.js With React Code

---
+++ Episode-1-Inception/3-Method/app.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("h1", {}, "Hello World From React");
+// - Creates an <h1> element
+// - No additional attributes (empty object {})
+// - The text content is set to "Hello World From React"
+
+// Get a reference to the root element:
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// - Selects the HTML element with the id "root"
+// - Prepares this element to be managed by React
+
+// Render the React element into the root element:
+root.render(heading);
+// - Displays the "Hello World From React" heading within the "root" element
