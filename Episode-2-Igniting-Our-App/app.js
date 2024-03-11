d357f16268038680d9b64e4cb39d99dec6574119
ameya-6964 <belvalkar7@gmail.com>
2024-03-11 18:57:59 +0530

    Completed Episode 2 Igniting Our App

--- Episode-2-Igniting-Our-App/app.js
+++ Episode-2-Igniting-Our-App/app.js
@@ -1,9 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { className: "child" }, [
-    React.createElement("h1", {}, "Hello Nested h1 Tag"),
-    React.createElement("h2", {}, "Hello Nested h2 Tag"),
+    React.createElement("h1", {}, "Hello Nested h1 Tag ✅"),
+    React.createElement("h2", {}, "Hello Nested h2 Tag 🚀"),
   ])
 );
 
