75bf8f14fb88497edd218cd8c0c01e8950c92552
ameya-6964 <belvalkar7@gmail.com>
2024-03-11 18:44:36 +0530

    Created Basic React Application With React CDN

---
+++ Episode-2-Igniting-Our-App/app.js
@@ -0,0 +1,11 @@
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { className: "child" }, [
+    React.createElement("h1", {}, "Hello Nested h1 Tag"),
+    React.createElement("h2", {}, "Hello Nested h2 Tag"),
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
