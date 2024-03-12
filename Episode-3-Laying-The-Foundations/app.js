88e7b78fd5f0f8851e9507fcd89ff4bd9d157a1d
ameya-6964 <belvalkar7@gmail.com>
2024-03-12 19:53:40 +0530

    Learned About JSX

--- Episode-3-Laying-The-Foundations/app.js
+++ Episode-3-Laying-The-Foundations/app.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1", { id: "parent" }, "Namaste React");
+
+// React.createElement => Creates JavaScript Object => After Render HTML
+const Heading = React.createElement("h1", { id: "parent" }, "Namaste React 🚀");
+
+// JSX => Javascript XML => Not HTML => HTML Like Syntax
+const JsxHeading = <h1 id="parent">Namaste React Using JSX 🚀</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(JsxHeading);
