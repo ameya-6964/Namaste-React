5a157830ce0dcb1fb9b1933f503276d0f8075dd6
ameya-6964 <belvalkar7@gmail.com>
2024-03-13 14:12:26 +0530

    Added JSX Heading

--- Episode-3-Laying-The-Foundations/app.js
+++ Episode-3-Laying-The-Foundations/app.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => Creates JavaScript Object => After Render HTML
+// React.createElement => Creates JavaScript Object => HTMLElement(render)
 const Heading = React.createElement("h1", { id: "parent" }, "Namaste React 🚀");
 
 // JSX => Javascript XML => Not HTML => HTML Like Syntax
+// JSX => Gets Transpiled To Normal React Syntax With Help Of babel transpiler
+
+// JSX => React.createElement => Creates JavaScript Object => HTMLElement(render)
 const JsxHeading = <h1 id="parent">Namaste React Using JSX 🚀</h1>;
+
+//Above Code Will gets transpiled To Pure React
+{
+  /* 
+  const JsxHeading = ("h1",
+  {id: "parent"},
+  "Namaste React Using JSX 🚀"
+  );
+  */
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(JsxHeading);
