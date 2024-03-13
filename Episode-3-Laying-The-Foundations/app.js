ef8c4b694b2b2813ef63395ea7990648a4e5e703
ameya-6964 <belvalkar7@gmail.com>
2024-03-13 16:07:14 +0530

    Understood Component Composition

--- Episode-3-Laying-The-Foundations/app.js
+++ Episode-3-Laying-The-Foundations/app.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 
 // React.createElement => Creates JavaScript Object => HTMLElement(render)
-const Heading = React.createElement("h1", { id: "parent" }, "Namaste React 🚀");
+const Heading = React.createElement(
+  "h1",
+  { id: "parent" },
+  "Namaste From React Create Element 🚀"
+);
 
 // JSX => Javascript XML => Not HTML => HTML Like Syntax
 // JSX => Gets Transpiled To Normal React Syntax With Help Of babel transpiler
-
 // JSX => React.createElement => Creates JavaScript Object => HTMLElement(render)
 const JsxHeading = <h1 id="parent">Namaste React Using JSX 🚀</h1>;
 
@@ -21,12 +24,25 @@ const JsxHeading = <h1 id="parent">Namaste React Using JSX 🚀</h1>;
 }
 
 const HeadingComponent = () => {
+  function getTime() {
+    return new Date().toUTCString();
+  }
+  const [time, setTime] = useState(getTime());
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(getTime());
+    }, 1000);
+    return () => clearInterval(intervalId); // Cleanup function to clear the interval
+  }, []); // Empty dependency array to ensure the effect runs only once
   return (
-    <h1 id="parent" tabIndex="3">
-      Namaste React From Functional Component
-    </h1>
+    <div id="parent" tabIndex="3">
+      <h1>Namaste React From Functional Component</h1>
+      {Heading}
+      {JsxHeading}
+      <h1>{time}</h1>
+    </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(JsxHeading);
+root.render(<HeadingComponent />);
