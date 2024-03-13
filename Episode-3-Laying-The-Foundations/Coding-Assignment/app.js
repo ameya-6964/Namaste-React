a82b9e939ec01ad05dbacbbc8be8bb57864de473
ameya-6964 <belvalkar7@gmail.com>
2024-03-13 18:33:09 +0530

    Completed The Assignment

--- Episode-3-Laying-The-Foundations/Coding-Assignment/app.js
+++ Episode-3-Laying-The-Foundations/Coding-Assignment/app.js
@@ -1,48 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
+import headerLogo from "./images/logo.webp";
+import profileLogo from "./images/dummy.jpg";
 
-// React.createElement => Creates JavaScript Object => HTMLElement(render)
-const Heading = React.createElement(
-  "h1",
-  { id: "parent" },
-  "Namaste From React Create Element 🚀"
+// Header Component
+const Header = () => (
+  <header className="header">
+    <img src={headerLogo} alt="Logo" className="logo" />
+    <input type="text" placeholder="Search Courses" className="search-bar" />
+    <div className="buttons">
+      <button className="login-button">Login</button>
+      <button className="signup-button">Sign Up</button>
+    </div>
+    <div className="user-icon">
+      <img src={profileLogo} alt="" />
+    </div>
+  </header>
 );
 
-// JSX => Javascript XML => Not HTML => HTML Like Syntax
-// JSX => Gets Transpiled To Normal React Syntax With Help Of babel transpiler
-// JSX => React.createElement => Creates JavaScript Object => HTMLElement(render)
-const JsxHeading = <h1 id="parent">Namaste React Using JSX 🚀</h1>;
-
-//Above Code Will gets transpiled To Pure React
-{
-  /* 
-  const JsxHeading = ("h1",
-  {id: "parent"},
-  "Namaste React Using JSX 🚀"
-  );
-  */
-}
-
-const HeadingComponent = () => {
-  function getTime() {
-    return new Date().toUTCString();
-  }
-  const [time, setTime] = useState(getTime());
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTime(getTime());
-    }, 1000);
-    return () => clearInterval(intervalId); // Cleanup function to clear the interval
-  }, []); // Empty dependency array to ensure the effect runs only once
-  return (
-    <div id="parent" tabIndex="3">
-      <h1>Namaste React From Functional Component</h1>
-      {Heading}
-      {JsxHeading}
-      <h1>{time}</h1>
-    </div>
-  );
-};
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<Header />);
