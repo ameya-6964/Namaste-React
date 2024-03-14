1f281c814206d56ff91a15a0976f89bf050696d8
ameya-6964 <belvalkar7@gmail.com>
2024-03-15 00:02:57 +0530

    Created Header And Added Tailwind CSS

--- Episode-4-Talk-Is-Cheap-Show-Me-The-Code/Lecture-Tasks/src/App.jsx
+++ Episode-4-Talk-Is-Cheap-Show-Me-The-Code/Lecture-Tasks/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="app">
+      {/* Header */}
+      <Header />
+    </div>
   );
 }
 
